c30d20ed682beb7ce7a07d4e4498467aa26d3aa5
wyw <wangyingwu@126.com>
2020-04-29 21:51:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,37 +10,12 @@
 #include "TcpServer.h"
 #include "HttpConnectionManager.h"
 
-//int main()
-//{
-//    EventLoop loop;
-//    TcpServer<HttpConnectionManager> myServer(&loop);
-//    myServer.start();
-//    loop.loop();
-//    return 0;
-//}
-
-#include <memory>
-#include <iostream>
-
-class A{
-public:
-    A(){};
-    virtual void run(){
-        std::cout<< "A"<<std::endl;
-    };
-};
-
-
-class B :public A{
-public:
-    B(){};
-    void run() override {
-        std::cout <<"B" <<std::endl;
-    }
-};
-int main(){
-    A a;
-    B b;
-    std::shared_ptr<A> c = std::make_shared<B>();
-    c->run();
+int main()
+{
+    EventLoop loop;
+    TcpServer<HttpConnectionManager> myServer(&loop);
+    myServer.start();
+    loop.loop();
+    return 0;
 }
+
